148d0e0d4bf0c85c5f6db9edd32003cf40ea9971
audsdunne <audsdunne@yahoo.co.uk>
2021-07-10 14:40:19 +0100

    Import datasets, and initial analysis

--- test.py
+++ test.py
@@ -3,3 +3,22 @@ import numpy as np
 import datetime
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+data_test = pd.read_csv("loan_data_test.csv")
+print(data_test)
+
+data_train = pd.read_csv("loan_data_train.csv")
+print(data_train)
+
+print(data_test.shape)
+print(data_test.info())
+print(data_test.describe().transpose)
+print(data_test.head(10))
+print(data_test.tail(10))
+
+
+print(data_train.shape)
+print(data_train.info())
+print(data_train.describe().transpose)
+print(data_train.head(5))
+print(data_train.tail(5))
